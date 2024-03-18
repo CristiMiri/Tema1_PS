2f2833e896a7e72d68236b0a815e18557fc5273e
Miritoiu Cristian <miritoiu.george@yahoo.com>
2024-03-18 15:08:55 +0200

    Prototype Dialogs and Login Page

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Tema1_PS.Model.Repository;
+using Tema1_PS.View;
+using Tema1_PS.View.ModalForms;
 
 namespace Tema1_PS
 {
@@ -26,17 +28,36 @@ namespace Tema1_PS
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
-            Console.WriteLine("Hello World!");
-            Repository repository = new Repository();
-            //SELECT * FROM public."Tester";
-            //print all the data from the table
-            DataTable dataTable =repository.ExecuteQuery("SELECT * FROM public.\"Tester\";");
-            foreach (DataRow row in dataTable.Rows)
-            {
-                Console.WriteLine(row["id"]);
-            }
-            MessageBox.Show("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Repository repository = new Repository();
+            ////SELECT * FROM public."Tester";
+            ////print all the data from the table
+            //DataTable dataTable =repository.ExecuteQuery("SELECT * FROM public.\"Tester\";");
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    Console.WriteLine(row["id"]);
+            //}
+            //MessageBox.Show("Hello World!");
 
+            //main.Content = new LoginPage();
+            
+            //UtilizatorDialog utilizatorDialog = new UtilizatorDialog();
+            //utilizatorDialog.ShowDialog();
+            
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            main.Content = new LoginPage();
+        }
+        private void Foms_Click(object sender, RoutedEventArgs e)
+        {
+            UtilizatorDialog utilizatorDialog = new UtilizatorDialog();
+            PrezentareDialog prezentareDialog = new PrezentareDialog();
+            ConferintaDialog conferintaDialog = new ConferintaDialog();
+            utilizatorDialog.ShowDialog();
+            prezentareDialog.ShowDialog();
+            conferintaDialog.ShowDialog();
         }
     }
 }
---
+++ View/ModalForms/UtilizatorDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tema1_PS.View.ModalForms
+{
+    /// <summary>
+    /// Interaction logic for UtilizatorDialog.xaml
+    /// </summary>
+    public partial class UtilizatorDialog : Window
+    {
+        public UtilizatorDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
